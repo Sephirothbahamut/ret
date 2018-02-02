882e6e033867ffd28994f847058337bbf7b7a965
Barnack <sephirothbahamut@live.it>
2018-02-02 23:47:20 +0100

    heroku tests

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.get('/count',function (req, res) {
 })
 
 app.post('/check',function (req, res) {
-	res.send(req.body);
 	checker(
 		req.body["url"],					//url
 		req.body["invocationParameters"],	//invocationParameters
